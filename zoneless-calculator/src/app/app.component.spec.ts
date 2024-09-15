5ca89a1590e1d98b3790de5f4d39c524d5e7f626
Aldair Bernal <aldair47x@Gmail.com>
2024-09-15 18:48:52 -0500

    Calculator tests progress

--- zoneless-calculator/src/app/app.component.spec.ts
+++ zoneless-calculator/src/app/app.component.spec.ts
@@ -1,11 +1,17 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+
+  let fixture: ComponentFixture<AppComponent>;
+  let compiled: HTMLElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    compiled = fixture.nativeElement as HTMLElement;
   });
 
   it('should create the app', () => {
@@ -14,16 +20,28 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'zoneless-calculator' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('zoneless-calculator');
+  it('Sould be 3', () => {
+    const num1 = 1;
+    const num2 = 2;
+    const sum = num1 + num2;
+    expect(sum).toBe(3);
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator');
+  it('Should render router-outlet', () => {
+    expect(compiled.querySelector('router-outlet')).not.toBeNull();
   });
+
+  it('Should render router-wrapped with css classes', () => {
+    const divElement = compiled.querySelector('div');
+    const mustHaveClasses = 'min-w-screen min-h-screen bg-slate-600 flex items-center justify-center px-5 py-5'.split(' ');
+    const divClasses = divElement?.classList.value.split(' ');
+    expect(divElement).not.toBeNull(); 
+
+    mustHaveClasses.forEach((className) => {
+      expect(divClasses).toContain(className);
+    });
+
+  });
+
+  
 });
